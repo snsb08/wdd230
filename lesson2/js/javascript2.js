0440182ac35ca6792f9a37c31397b2aa7427909b
snsb08 <snsb08@gmail.com>
2022-01-15 15:06:56 -0500

    updated page with media query

---
+++ lesson2/js/javascript2.js
@@ -0,0 +1,8 @@
+let date_updated = document.lastModified;
+
+let lastModif = new Date(document.lastModified);
+
+let time_updated
+
+const dateModified = document.querySelector("#dateModified");
+dateModified.textContent =" Last Updated:" + document.lastModified;
