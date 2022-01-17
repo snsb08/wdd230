03ba595560e9d783bf65efc6ccaf7a8cca546c23
snsb08 <snsb08@gmail.com>
2022-01-17 15:19:21 -0500

    fixed year updates

--- lesson2/js/javascript2.js
+++ lesson2/js/javascript2.js
@@ -1,8 +1,11 @@
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('currentYear').innerHTML = "\u00A9" + year + " .*| Sheyla N. Norton |*. Florida";
+
 let date_updated = document.lastModified;
 
 let lastModif = new Date(document.lastModified);
 
-let time_updated
 
 const dateModified = document.querySelector("#dateModified");
 dateModified.textContent =" Last Updated:" + document.lastModified;
