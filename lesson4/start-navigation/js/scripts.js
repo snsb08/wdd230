3746ae7a9c88db4d929ae4eed16e9247b199ae9a
snsb08 <snsb08@gmail.com>
2022-01-26 11:55:48 -0500

    added normalize file and meta descriptions to the site plan

---
+++ lesson4/start-navigation/js/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu () {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
+}
+
+const x= document.getElementById('hamburguerBtn');
+x.onclick = toggleMenu;
