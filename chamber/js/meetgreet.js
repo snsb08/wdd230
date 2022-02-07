e7438e7e2892fded522851538877638fa3e00594
snsb08 <snsb08@gmail.com>
2022-02-07 08:36:16 -0500

    added photo, banner, link on portal

---
+++ chamber/js/meetgreet.js
@@ -0,0 +1,14 @@
+const today = new Date();
+let dayToday;
+
+dayToday = today.getDay();
+let chamberMessage; 
+
+if (dayToday >= 1 && dayToday <=2) {
+    chamberMessage = "🤝 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+    document.querySelector('#monTue').innerHTML = chamberMessage;
+}  else {
+    document.getElementById("#monTue").style.display = "block";
+    // chamberMessage = "None"
+}
+
