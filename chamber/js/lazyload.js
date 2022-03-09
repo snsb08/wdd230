5b951500eb09417ca887a629ba1caefecbc9e17c
snsb08 <snsb08@gmail.com>
2022-03-09 13:11:11 -0500

    discovery lazy load fixed, hero img changed and other updates

--- chamber/js/lazyload.js
+++ chamber/js/lazyload.js
@@ -1,68 +1,69 @@
-const imagesToLoad = document.querySelectorAll("[data-src]");
-
-function loadingImages(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-    img.setAttribute("src", src);
-}
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
     threshold: 1,
-    rootMargin: "0px 0px 300px 0px"
-    
+    rootMargin: '0px 0px 100px 0px'
 };
 
-const imgObserver = new IntersectionObserver ((entries, imgObserver) => {
-    entries.forEach(entry =>{
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            loadingImages(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
+const loadingImages = (image) => {
+    image.setAttribute("src", image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    }; 
+};
 
-imagesToLoad.forEach (image => {
-    imgObserver.observe(image);
-    loadingImages(image);
-})
-// console.log(imagesToLoad);
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+              loadingImages(item.target);
+              observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+
+    imagesToLoad.forEach((img) => {
+      imgObserver.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadingImages(img);
+    });
+  }
 
 
 
+// function loadingImages(img) {
+//     const src = img.getAttribute("data-src");
+//     if(!src) {
+//         return;
+//     }
+//     img.setAttribute("src", src);
+// };
 
 // const imgOptions = {
 //     threshold: 1,
-//     rootMargin: "0px 0px 50px 0px"
+//     rootMargin: "0px 0px 30px 0px"
+    
 // };
 
-// const loadingImages = (image) => {
-//     image.setAttribute('src', image.getAttribute('data-src'));
-//     image.onload = () => {image.removeAttribute('data-src');}; 
-// };
+// const imgObserver = new IntersectionObserver ((entries, imgObserver) => {
+//     entries.forEach(entry =>{
+//         if (!entry.isIntersecting) {
+//             return;
+//         } else {
+//             loadingImages(entry.target);
+//             imgObserver.unobserve(entry.target);
+//         }
+//     })
+// }, imgOptions)
 
-// if('IntersectionObserver' in window) {
-//     const imgObserver = new IntersectionObserver((items, imgObserver) => {
-//         items.forEach((item) => {
-//             if (item.isInsersecting) {
-//             loadingImages(item.target);
-//             imgObserver.unobserve(item.target);}
-//         });
-//     }, imgOptions);
+// imagesToLoad.forEach (image => {
+//     imgObserver.observe(image);
+//     loadingImages(image);
+// });
 
-//     //loop through each img
-//     imagesToLoad.forEach((img) => {
-//         imgObserver.observe(img);
-//     });
+// console.log(imagesToLoad);
 
 
-// }
-// else {
-//     //just load All images if not supported
-//     imagesToLoad.forEach((img) => {
-//         loadingImages(img);
-//     });
-// }
