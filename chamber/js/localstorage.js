c488d27ac18712ad44fae042e8cb27ec16d25018
snsb08 <snsb08@gmail.com>
2022-03-19 14:20:07 -0400

    testing visit page function

--- chamber/js/localstorage.js
+++ chamber/js/localstorage.js
@@ -1,43 +1,74 @@
 
-const visits = document.querySelector(".visits");
+// const visits = document.querySelector(".visits");
 
-let visitAmount = Number(window.localStorage.getItem("visited"));
+// let visitAmount = Number(window.localStorage.getItem("visited"));
 
-// ------Times visited--------
-// if (visitAmount !==0) {
-//     visits.textContent = visitAmount + " times";
+// // ------Times visited--------
+// // if (visitAmount !==0) {
+// //     visits.textContent = visitAmount + " times";
+// // } else {
+// //     visits.textContent = `For the First time!`;
+// // }
+
+// // //increment the number of visits:
+
+// // visitAmount++;
+// // localStorage.setItem("visits-ls", visitAmount);
+
+
+// //-----DAYS visited:--------------
+
+// // const visits = window.localStorage.getItem("visits");
+
+// const convertDay = 1000 * 60 * 60 * 24;
+
+// let days = Date.now() - visitAmount //this is in milliseconds 
+// let dayAmount = days / convertDay;
+
+// // window.localStorage.setItem('visited', new Date(Date.now()));//not need window 
+
+// let visitDays = Math.floor(dayAmount); // for whole number
+// // document.getElementById('visits').textContent = dayAmount;
+
+// if (visitDays !==0) {
+//     visits.textContent = visitDays + " day(s)";
 // } else {
 //     visits.textContent = `For the First time!`;
 // }
 
-// //increment the number of visits:
+// // visitDays++;
+// localStorage.setItem("visited", visitDays);
+// // localStorage.setItem("visited", Date.now()); //this or the one above it?
 
-// visitAmount++;
-// localStorage.setItem("visits-ls", visitAmount);
+
+//try again:---------------------------------------------
 
 
-//-----DAYS visited:--------------
 
-// const visits = window.localStorage.getItem("visits");
+const visits = document.querySelector("#visits"); // location to display on page
+const visitAmount = window.localStorage.getItem("visited"); //took off Number and no change
 
-const convertDay = 1000 * 60 * 60 * 24;
+const lastVisit = Date.parse(visitAmount);
 
-let days = Date.now() - visitAmount //this is in milliseconds 
+// const convertDay = 1000 * 60 * 60 * 24;
+const convertDay = 1000 * 60 * 60;
+
+let days = Date.now() - lastVisit //this is in milliseconds 
 let dayAmount = days / convertDay;
 
-// window.localStorage.setItem('visited', new Date(Date.now()));//not need window 
+window.localStorage.setItem('visited', new Date(Date.now()));//was I missing this only? i had it commented, why?
 
 let visitDays = Math.floor(dayAmount); // for whole number
 // document.getElementById('visits').textContent = dayAmount;
 
-if (visitDays !==0) {
+if (visitDays !== 0) {
     visits.textContent = visitDays + " day(s)";
 } else {
     visits.textContent = `For the First time!`;
 }
 
-visitDays++;
-// localStorage.setItem("visited", visitDays);
-localStorage.setItem("visited", Date.now()); //this or the one above it?
 
+// visitDays++;
+// localStorage.setItem("visited", visitDays);
+// localStorage.setItem("visited", Date.now()); //this or the one above it?
 
